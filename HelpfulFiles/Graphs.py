3851f2c43124bc7d8445ba1c5c663ec666692a11
Alex Hamlet <alexhamlet@comcast.net>
2020-06-06 01:25:51 -0600

    Finished ESDGraph and Day 9 pt 1.  Part 2 cannot use the generous method

--- HelpfulFiles/Graphs.py
+++ HelpfulFiles/Graphs.py
@@ -1,7 +1,13 @@
+#!usr/bin/python3
 
-class SymbolEdgeWeightedDiGraph:
+#Useful for parsing files
+import re
+
+#EdgeWeightedSymbolDiGraph
+class ESDGraph:
+
+    #Init number of Vertices, Edges, and Adjecency lists
     def __init__(self):
-        #Init number of Vertices, Edges, and Adjecencies
         self.V = 0
         self.E = 0
         self.adj = {}
@@ -23,28 +29,50 @@ class SymbolEdgeWeightedDiGraph:
             self.adj[V2][V1] = W
             self.E += 1
 
-    def getAdj(self,V):
-        self.validateVertice(V)
-        return self.adj.get(V)
-
+    #Returns the number of adjacent vertices
     def degree(self,V):
         return len(self.adj[V])
 
+    #Determines if the verticy already exists
+    #If not an exception is raised
     def validateVertice(self,V):
         if(self.adj.get(V) == None):
             raise Exception(f"{V} is not a valid vertice.")
 
+    #Creates a graph from a file
+    #delim is a regex string to split places on
+    def GraphFromFile(self, file, delim=",|to|="):
+        #Read file
+        file = open(file)
+        input = file.readlines()
+        file.close()
+
+        #prepare delim regex
+        delim = re.compile(delim)
+
+        #For each entry add the vertice and edge
+        for line in input:
+            con = re.split(delim, line.replace(" ", "").strip())
+            self.addVertice(con[0])
+            self.addVertice(con[1])
+            self.addEdge(con[0], con[1], con[2])
+            self.addEdge(con[1], con[0], con[2])
+
+    #Prints a tiered list of Vertices, Adjacencies, and Weights
     def __str__(self):
         retstr = f"{self.V} Vertices, {self.E} edges\n"
         for v in self.adj:
-            retstr += f"{v}:\n\t{self.adj[v]}\n"
+            retstr += f"{v}:\n"#\t{self.adj[v]}\n
+            for e in self.adj[v]:
+                retstr += f"\t{e}\t:\t{self.adj[v][e]}\n"
         return retstr
 
+    #Dinky tests
     def main():
         #Notify User Main is running
         print("running Graph main")
         #Make a Graph!
-        G = SymbolEdgeWeightedDiGraph()
+        G = ESDGraph()
         G.addVertice("a")
         G.addVertice("b")
         G.addVertice("c")
@@ -55,4 +83,4 @@ class SymbolEdgeWeightedDiGraph:
 
 #Runs only if run directly
 if __name__ == "__main__":
-    Graph.main()
+    ESDGraph.main()
